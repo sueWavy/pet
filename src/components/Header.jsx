e081d921a39b66f219df88853c8b9d290cd03e99
sueWavy <sueboi0311@gmail.com>
2023-11-06 15:28:33 +0900

    test : 서버단 데이터 받아와지는지 테스트 ( 200 성공 )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { MdPets } from "react-icons/md";
 import Weather from "./Weather";
+import Navbar from "./Navbar";
 
 export default function Header() {
   return (
     <>
-      <header className="flex items-center justify-center text-white text-3xl p-4 bg-brand font-[KCCChassam] ">
-        <MdPets />
-        <h1 className="ml-2">같이 산책할땐 멍개팅</h1>
+      <header className="flex items-center justify-around text-white text-3xl p-4 bg-brand font-[KCCChassam] ">
+        <div className="flex">
+          <MdPets />
+          <h1 className="ml-2">같이 산책할땐 멍개팅</h1>
+        </div>
+        <Navbar />
       </header>
       <div className="">
         <Weather />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const form = new FormData();
+
+    form.append("mode", "test");
+
+    axios
+      .post("http://43.201.39.118/api/main.php", form, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((data) => console.log(data));
+  }, []);
+
   return <div>Home</div>;
 }
