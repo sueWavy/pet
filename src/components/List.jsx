f944c3d7e8f551c64e281d074a87f11d9e84fc23
sueWavy <sueboi0311@gmail.com>
2023-11-22 19:55:08 +0900

    edit : ListCard 레이아웃 변경

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,7 +6,7 @@ export default function List() {
   const data = dummyData;
 
   return (
-    <div className="grid grid-cols-4 gap-2 md:grid-cols-2 s:grid-cols-1">
+    <div className="grid grid-cols-4 gap-2 p-3 bg-slate-200 md:grid-cols-2 s:grid-cols-1">
       {data.map((it) => (
         <ListCard key={it.id} data={it} />
       ))}
--- src/components/ui/ListCard.jsx
+++ src/components/ui/ListCard.jsx
@@ -1,25 +1,51 @@
 import React from "react";
+import { GoHeart } from "react-icons/go";
 
 export default function ListCard({
   data,
-  data: { id, title, userName, date, time, likes, address, img, map, content },
+  data: {
+    id,
+    title,
+    userName,
+    petName,
+    date,
+    time,
+    likes,
+    address,
+    img,
+    map,
+    content,
+  },
 }) {
   const newDate = date.toString();
 
   return (
-    <div className="border-solid border-slate-700 max-w-xl border-2">
-      <h3>{title}</h3>
-      <div>
-        <p>{userName}</p>
-        <p>{newDate}</p>
-        <p>좋아요 : {likes}</p>
-      </div>
-      <div>
-        <p>{address}</p>
-        <p>{time}</p>
+    <div className="border-solid border-slate-700 max-w-xl border-2 text-center p-2 bg-white rounded-2xl">
+      <div className="p-2">
+        <h3 className="flex justify-around border-solid border-slate-700 border-2 p-2 rounded-2xl">
+          {title}
+          <span className="flex items-center gap-2">
+            <span className="cursor-pointer">
+              <GoHeart />
+            </span>
+            {likes}
+          </span>
+        </h3>
+        <div className="mt-2">
+          <p>{newDate}</p>
+        </div>
+        <div className="flex justify-center">
+          <p className="mr-5">{address}</p>
+          <p>{time}</p>
+        </div>
+        <div className="flex justify-center">
+          <p>{userName}</p>
+          <p className="mr-2 ml-2">|</p>
+          <p>{petName}</p>
+        </div>
       </div>
       <img src={img} alt="사진" />
-      <p>{map}</p>
+      <p className="border-solid border-slate-700 border-b-2">{map}</p>
       <p>{content}</p>
     </div>
   );
